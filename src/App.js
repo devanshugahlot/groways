f43c17849317b74093da09b6b6c8c8ad4afceed9
devanshugahlot <devanshugahlot79@gmail.com>
2024-03-18 13:21:34 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import DrawerAppBar from "./component/Navbar";
+import Show from "./component/Show";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <DrawerAppBar /> */}
+      <Show />
+    </>
   );
 }
 
---
+++ src/component/Show.jsx
@@ -0,0 +1,234 @@
+import React from "react";
+import "./show.css";
+const Show = () => {
+  return (
+    <div style={{ background: "black" }}>
+      <main className="pt-20 bg-backimg bg-no-repeat bg-cover">
+        <div className="flex items-center justify-center flex-col">
+          <div className="h-full w-full my-5 mt-0 flex justify-center items-center overflow-hidden">
+            <div className="flex justify-center items-center">
+              <div className="flex flex-col justify-center items-center animate-spin-clock">
+                <div className="flex justify-end items-end gap-7 mb-[-10px] md:mb-[10px] md:gap-11">
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 undefined"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/image0f9a.png 1x,
+                    ./images/image0f9a.png 2x
+              "
+                    src="./images/image0f9a.png"
+                  />
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 mb-9 md:mb-16"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/image2b7f.png  1x,
+                    ./images/image2b7f.png 2x
+              "
+                    src="./images/image2b7f.png"
+                  />
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 undefined"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/image564a.png 1x,
+                    ./images/image564a.png 2x
+              "
+                    src="./images/image564a.png"
+                  />
+                </div>
+                <div className="flex justify-center items-center gap-7 md:gap-20">
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 rounded-none"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/image7641-removebg-preview.png 1x,
+                  ./images/image7641-removebg-preview.png  2x
+              "
+                    src="./images/image7641-removebg-preview.png"
+                  />
+                  <div className="h-32 w-32 md:h-52 md:w-52 bg-yellow-400 rounded-full shadow-[0_0_84px_orange] shadow-yellow-500 flex items-center justify-center animate-spin-clock direction-reverse">
+                    <svg
+                      fill="hsl(var(--background))"
+                      version="1.1"
+                      id="Layer_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      viewBox="0 0 256 178"
+                      width={100}
+                      height={100}
+                      enableBackground="new 0 0 256 178"
+                      xmlSpace="preserve"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+
+                      <g id="SVGRepo_iconCarrier">
+                        <text
+                          style={{ fontWeight: "bold" }}
+                          x="50%"
+                          y="50%"
+                          dominant-baseline="middle"
+                          text-anchor="middle"
+                          font-size="74"
+                          textLength="200"
+                          lengthAdjust="spacingAndGlyphs"
+                        >
+                          Growways.io
+                        </text>
+                      </g>
+                    </svg>
+                  </div>
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 rounded-none"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/imagea2a4-removebg-preview.png  1x,
+                  ./images/imagea2a4-removebg-preview.png 2x
+              "
+                    src="./images/imagea2a4-removebg-preview.png"
+                  />
+                </div>
+                <div className="flex justify-end items-start gap-7 md:gap-11 mt-[-10px] md:mt-[10px]">
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 undefined"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/imagea08f-removebg-preview.png  1x,
+                    ./images/imagea08f-removebg-preview.png 2x
+              "
+                    src="./images/imagea08f-removebg-preview.png"
+                  />
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 mt-9 md:mt-16"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/imagedf25.png  1x,
+                    ./images/imagedf25.png 2x
+              "
+                    src="./images/imagedf25.png"
+                  />
+                  <img
+                    alt="planet"
+                    loading="lazy"
+                    width={60}
+                    height={60}
+                    decoding="async"
+                    data-nimg={1}
+                    className="rounded-full h-15 w-15 md:h-28 md:w-28 undefined"
+                    style={{ color: "transparent" }}
+                    srcSet="
+                    ./images/imagee5b8.png  1x,
+                    ./images/imagee5b8.png 2x
+              "
+                    src="./images/imagee5b8.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center flex-col my-5 mt-10 w-full">
+            <div className="rounded-lg border text-card-foreground p-4 md:w-4/5 shadow-md bg-secondary">
+              <div className="flex flex-col justify-center items-center md:flex-row w-full md:gap-10">
+                <div className="w-52 h-52 md:w-64 md:h-64">
+                  <img
+                    alt="Tether BNB IMG"
+                    fetchpriority="high"
+                    width={300}
+                    height={300}
+                    decoding="async"
+                    data-nimg={1}
+                    style={{
+                      color: "transparent",
+                      width: "100%",
+                      height: "100%",
+                    }}
+                    srcSet="
+                ./images/imagef56e.png 1x,
+                ./images/imagef56e.png 2x
+              "
+                    src="./images/imagef56e.png"
+                  />
+                </div>
+                <h1 className="text-4xl sm:text-5xl sm:mb-11 sm:ml-[-1.5rem] text-center font-black sm:leading-snug whitespace-nowrap bg-clip-text text-transparent bg-gradient-to-b from-yellow-500 to-green-800">
+                  groways.io <br />
+                  is on <br />
+                  BEP20 USDT
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full my-5">
+            <h2
+              style={{ color: "white" }}
+              className="text-center font-bold text-3xl"
+            >
+              <span className="text-primary">User Friendly</span> Dashboard
+            </h2>
+            <div className="mt-4 w-full flex justify-center items-center p-4 max-w-3xl mx-auto">
+              <img
+                alt="Dashboad inside Laptop looks"
+                loading="lazy"
+                width={1920}
+                height={1080}
+                decoding="async"
+                data-nimg={1}
+                style={{ color: "transparent" }}
+                src="./images/Modern_illustrated_Programming_and_Software_Instagram_Post___1_-removebg-preview.png"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Show;
